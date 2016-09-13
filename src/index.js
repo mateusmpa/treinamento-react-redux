24410c3a50b01b15db35caeb033b860087bd2f39
Mateus Alves <mateusmpa@gmail.com>
2016-09-13 19:48:32 -0300

    components react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const = Player = () =>
-  <>
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+  <p>
+  </p>
 )
+
+render(<Novo />, document.getElemenById('app-podcast'))
